8393d658ce522b6cd4e3f0c39da8229a59aee792
Nick Devos <145980327+FoxIt96@users.noreply.github.com>
2023-12-10 13:34:04 +0100

    datamodellen

---
+++ app/models.py
@@ -0,0 +1,8 @@
+class Transaction:
+    def __init__(self, description, amount):
+        self.description = description
+        self.amount = amount
+
+class Category:
+    def __init__(self, name):
+        self.name = name
