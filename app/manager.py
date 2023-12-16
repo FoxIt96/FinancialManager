263546d7a5583bd1ac435f73335b85f9fe20b298
Nick Devos <145980327+FoxIt96@users.noreply.github.com>
2023-12-16 10:48:06 +0100

    Categorieën toegevoegd
    models --> category_id
    manager --> add_category +get_all_categories

--- app/manager.py
+++ app/manager.py
@@ -8,17 +8,35 @@ class FinancialManager:
 
     def create_tables(self):
         self.db.execute_query('''
+            CREATE TABLE IF NOT EXISTS categories (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT
+            )
+        ''')
+        self.db.execute_query('''
             CREATE TABLE IF NOT EXISTS transactions (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 description TEXT,
-                amount REAL
+                amount REAL,
+                category_id INTEGER,
+                FOREIGN KEY(category_id) REFERENCES categories(id)
             )
         ''')
 
     def add_transaction(self, transaction):
-        query = "INSERT INTO transactions (description, amount) VALUES (?, ?)"
-        self.db.execute_query(query, (transaction.description, transaction.amount))
+        query = "INSERT INTO transactions (description, amount, category_id) VALUES (?, ?, ?)"
+        self.db.execute_query(query, (transaction.description, transaction.amount, transaction.category_id))
 
     def get_all_transactions(self):
         self.db.execute_query("SELECT * FROM transactions")
         return self.db.fetch_all()
+
+    def add_category(self, category):
+        query = "INSERT INTO categories (name) VALUES (?)"
+        self.db.execute_query(query, (category.name,))
+
+    def get_all_categories(self):
+        self.db.execute_query("SELECT * FROM categories")
+        return self.db.fetch_all()
+
+   
--- app/models.py
+++ app/models.py
@@ -1,7 +1,8 @@
 class Transaction:
-    def __init__(self, description, amount):
+    def __init__(self, description, amount, category_id):
         self.description = description
         self.amount = amount
+        self.category_id = category_id
 
 class Category:
     def __init__(self, name):
